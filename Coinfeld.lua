ef232b554ebdccb6721a3cd419ba8856ce395a45
Donald Tang <tangyfruits@gmail.com>
2016-08-03 00:19:46 +1200

    Hardcoded coin throwing between me and Stonesnake

--- Coinfeld.lua
+++ Coinfeld.lua
@@ -1,15 +1,26 @@
+snakecount = 0
+tincount = 0
 local Coin_EventFrame = CreateFrame("Frame")
-count = 0
-Coin_EventFrame:RegisterEvent("CHAT_MSG_LOOT")
+Coin_EventFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 Coin_EventFrame:SetScript("OnEvent",
     function(self, event, ...)
-        local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11 = ...
-        print("hi")
-        if args1:match("Counterfeit Coin") then
-            count = count + 1
-            print(count)
+        local args1, args2, args3, args4, args5, args6, args7, args8, args9, args10, args11, args12 = ...
+        if(args5 == UnitName("player") and args9 == "Stonesnake" and args12 == 173930) then
+            SendChatMessage("You have thrown the Coin " .. tincount .. " times.", nil, nil)
+            if math.fmod(snakecount, 13) == 1 then
+                SendChatMessage("You have thrown the Coin " .. tincount .. " times.", nil, nil)
+            end
+        end
+        if(args9 == UnitName("player") and args5 == "Stonesnake" and args12 == 173930) then
+            snakecount = snakecount + 1
+            SendChatMessage("You have caught the Coin " .. snakecount .. " times.", nil, nil)
+            if math.fmod(snakecount, 13) == 1 then
+                SendChatMessage("You have caught the Coin " .. snakecount .. " times.", nil, nil)
+            end
         end
     end)
+
+
 function Coin_EventFrame:BAMBOOZLEMEWITHCOINS()
     print ("test")
 end
